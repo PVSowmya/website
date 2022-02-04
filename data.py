4290c7cf7d2941b76f7467a7a850a64bd2eb845e
Venkata Sowmya Pinni <sowmyavenkata777@gmail.com>
2022-02-04 17:08:00 +0530

    flaskapp

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'python',
+            'body':'This article consists of python topics',
+            'author':'John Doe',
+            'created_date':'04-05-2021'
+        },
+        {
+            'id':2,
+            'title':'java',
+            'body':'This article consists of java topics',
+            'author':'Michael',
+            'created_date':'22-10-2021'
+        },
+        {
+            'id':3,
+            'title':'Sql',
+            'body':'This article consists of sql topics',
+            'author':'david',
+            'created_date':'07-07-2021'
+        }
+    ]
+    return articles
