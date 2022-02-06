67ffa85af83344e9c7c3b0e4601e36414191f1f4
PVSowmya <91245554+PVSowmya@users.noreply.github.com>
2022-02-07 00:06:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_mysqldb import *
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 from functools import wraps
+import re
 
 app = Flask(__name__)
 
@@ -12,7 +13,6 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'demo'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-# init MYSQL
 
 mysql = MySQL(app)
 
@@ -58,26 +58,33 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods =['GET', 'POST'])
 def register():
     form=RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name=form.name.data
-        email=form.email.data
-        username=form.username.data
-        password=sha256_crypt.encrypt(str(form.password.data))
-
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-        mysql.connection.commit()
-        cur.close()
-
-        flash('You are now registered and can login', 'success')
-
-        return redirect(url_for('register'))
-        
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        name=request.form['name']
+        password = sha256_crypt.encrypt(str(request.form['password']))        
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE username = % s or email=%s', (username,email ))
+        account = cursor.fetchone()
+        if account:
+            flash('Account already exists !','danger')
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('Invalid email address !','danger')
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            flash('Username must contain only characters and numbers !','danger')
+        elif not username or not password or not email:
+            flash('Please fill out the form !','danger')
+        else:
+            cursor.execute('INSERT INTO users VALUES (NULL, % s, % s, % s,%s)', (name,email,username, password, ))
+            mysql.connection.commit()
+            flash('You have successfully registered !','success')
+    elif request.method == 'POST':
+        flash('Please fill out the form !','danger')
     return render_template('register.html',form=form)
-
 
 @app.route('/login',methods=['GET','POST'])
 def login():
